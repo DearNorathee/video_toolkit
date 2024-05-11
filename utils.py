50219ee4a5cf45558874549de68a657c2bf89967
NoratheeDear <noratee5@hotmail.com>
2024-05-11 09:56:58 +0700

    remove tests from the main function file

--- utils.py
+++ utils.py
@@ -1347,57 +1347,6 @@ def extract_audio(video_folder:     Union[Path,str],
     return video_name_list
        
 
-def test_extract_1_audio():
-    
-    folder = Path(r"E:\Videos\The Big Bang Theory\The Big Bang Theory French Season 06")
-    video_name = "The Big Bang Theory French S06E01.mp4"
-    video_path = folder / video_name
-    output_folder = Path(r"C:\Users\Heng2020\OneDrive\Python NLP\NLP 06_ffmpeg")
-    output_name = "The Big Bang Theory French S06E01.mp3"
-    output_path = output_folder / output_name
-    
-    extract_1_audio(video_path,output_folder,output_name)
-    extract_1_audio(video_path,output_folder,output_name,overwrite_file = False)
-
-def test_extract_audio():
-    
-    from pathlib import Path
-    French_bigbang = Path(r"E:\Videos\The Big Bang Theory\The Big Bang Theory French Season 06")
-    output_folder = Path(r"E:\Videos\The Big Bang Theory\The Big Bang Theory French Season 06\Audio")
-    extract_audio(French_bigbang,output_folder,n_limit=10)
-    return True
-
-def test_crop_video():
-    from pathlib import Path
-    video_path = r"C:\Users\Heng2020\OneDrive\Python NLP\InputData\Westworld S04E01 Portuguese.mkv"
-    t1 = "0:02:25"
-    t2 = "0:06:00"
-    
-    crop_video(video_path,t1,t2)
-
-def test_create_subtitle():
-    import whisper
-    from whisper.utils import get_writer
-    from playsound import playsound
-    from time import time
-    
-    alarm_done_path = r"H:\D_Music\Sound Effect positive-logo-opener.mp3"
-    
-    ts01 = time()
-    
-    input_path = r"C:\Users\Heng2020\OneDrive\Python NLP\InputData\Westworld S04E01 Portuguese_01.mkv"
-    
-    model = whisper.load_model("base")
-    result = model.transcribe(input_path)
-    
-    output_directory = r"C:\Users\Heng2020\OneDrive\Python NLP\InputData\Westworld S04E01 Portuguese_01.srt"
-    
-    writer = get_writer("srt", str(output_directory))
-    writer(result, output_name)
-    ts02 = time()
-    
-    duration = ts02 - ts01 
-    print(duration)
 
 
 def srt_to_df(srt_path,
